ab402b704c45269047ef9e1ff87e13ee496ef7c4
chaehyeo-n <parkche03@naver.com>
2024-09-22 01:11:01 +0900

    Fix: photo 필드 text에서 image로 변경

--- BackEnd/qrcode_app/models.py
+++ BackEnd/qrcode_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Photo 모델 생성
 class Photo(models.Model):
     id = models.AutoField(primary_key=True)
-    photo = models.TextField(blank=True)    # 네컷 사진
+    photo = models.ImageField(blank=True)    # 네컷 사진
     created_at = models.DateTimeField(auto_now_add=True)    # 생성 날짜
     qr_code = models.ImageField(upload_to='QRcode/', blank=True, null=True)
     # 그냥 항상 유저 정보 불일치 값을 넣어놓고 링크 이동 못하게 해야겠다
