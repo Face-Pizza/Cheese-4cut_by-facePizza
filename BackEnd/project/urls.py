a6a675c2d50eb149f68c88230a5827bba6cdf9ba
chaehyeo-n <parkche03@naver.com>
2024-09-22 01:11:26 +0900

    Fix: project urls.py에도 이미지 경로 추가

--- BackEnd/project/urls.py
+++ BackEnd/project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('qrcode_app.urls')),
 ]
+
+if settings.DEBUG:  # 개발 중일 때만
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
