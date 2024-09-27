1bef8161608a80a2669c88b666e05badcf0af644
iam-hyo <hyojunj@naver.com>
2024-09-28 03:29:45 +0900

    [feat]네컷만화 기능구현

--- FrontEnd/src/pages/PrintPage.jsx
+++ FrontEnd/src/pages/PrintPage.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import * as Pr from '../styles/printStyle';
+import Cartoon1 from '../assets/cartoon/Catoon_1.png';
+import Cartoon2 from '../assets/cartoon/Catoon_2.png';
+import Cartoon3 from '../assets/cartoon/Catoon_3.png';
+import Cartoon4 from '../assets/cartoon/Catoon_4.png';
 
 const PrintPage = ({setCapturedPhotos ,savedImage}) => {
   const navigate = useNavigate();
-  // const { savedImage } = location.state || {};
-  const [timer, setTimer] = useState(6); // 타이머 상태 (6초 후 이동)
+  const [timer, setTimer] = useState(30); // 타이머 상태 (20초 후 이동)
+  const cartoons = [Cartoon1, Cartoon2, Cartoon3, Cartoon4]; // 카툰 이미지 배열
+  const [currentImageIndex, setCurrentImageIndex] = useState(0); // 현재 카툰 인덱스
   
   useEffect(() => {
     // 1초마다 타이머를 감소시키는 인터벌 설정
@@ -26,16 +32,21 @@ const PrintPage = ({setCapturedPhotos ,savedImage}) => {
     return () => clearInterval(countdown);
   }, [navigate, setCapturedPhotos]);
 
+  useEffect(() => {
+    // 5초마다 카툰 이미지 변경
+    const changeCartoon = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % cartoons.length); // 인덱스를 순환시킴
+    }, 5000); // 5초마다 실행
+
+    return () => clearInterval(changeCartoon); // 컴포넌트 언마운트 시 타이머 정리
+  }, [cartoons.length]);
+
   return (
-    <>
-      <div>프린트 중입니다...</div>
-      <div>{timer}초 후에 메인 페이지로 이동합니다.</div>
-      {savedImage ? (
-        <img src={savedImage} alt="Saved preview" />
-      ) : (
-        <p>저장된 사진이 없습니다.</p>
-      )}
-    </>
+    <Pr.PrintPage>
+      <h1>프린트 중입니다...</h1>
+      <h3>{timer}초 후에 메인 페이지로 이동합니다.</h3>
+      <Pr.Cartoon src={cartoons[currentImageIndex]} />
+    </Pr.PrintPage>
   );
 };
 
---
+++ FrontEnd/src/styles/printStyle.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const PrintPage = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    background-color: #FFFCEE;
+    height: 100vh;
+
+    h1{
+        margin: 130px 0 10px 0;
+        font-size: 55px;
+    }
+
+    h3{
+        font-size: 30px;
+        margin: 0 0 30px 0;
+`
+
+export const Cartoon = styled.img`
+    width: 1372PX;
+`
